bf387e088e3f964b53b5a4cb9b05580604f8a86f
wonseok <dnjstjr245@gmail.com>
2022-11-07 19:35:13 +0900

    [feat] shared 모듈의 build.gradle.kts 초기화 (#1)

---
+++ shared/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+}
+
+android {
+    buildFeatures {
+        dataBinding = true
+    }
+    namespace = "com.yangbong.damedame.shared"
+}
+
+dependencies {
+    implementation(project(":domain"))
+    implementation(project(":data"))
+
+    // Android Core
+    implementation(AndroidXDependencies.coreKtx)
+    implementation(AndroidXDependencies.appCompat)
+    implementation(AndroidXDependencies.constraintLayout)
+    implementation(AndroidXDependencies.legacy)
+    implementation(AndroidXDependencies.coroutines)
+    implementation(AndroidXDependencies.splashScreen)
+
+    // Kotlin
+    implementation(KotlinDependencies.kotlin)
+
+    implementation(AndroidXDependencies.coreKtx)
+
+    // Material Design
+    implementation(MaterialDesignDependencies.materialDesign)
+}
