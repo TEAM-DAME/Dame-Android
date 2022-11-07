e25e49894e0138dd172830d5d35704ff33598737
wonseok <dnjstjr245@gmail.com>
2022-11-07 19:02:41 +0900

    [feat] buildSrc 모듈 초기화 (#1)

---
+++ buildSrc/src/main/java/Constants.kt
@@ -0,0 +1,8 @@
+object Constants {
+    const val packageName = "com.yangbong.damedame"
+    const val compileSdk = 32
+    const val minSdk = 23
+    const val targetSdk = 32
+    var versionCode = 1
+    var versionName = "1.0.0"
+}
