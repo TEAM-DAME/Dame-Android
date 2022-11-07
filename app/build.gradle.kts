7177002f271a53d8c7165d0588034a0314b190ef
wonseok <dnjstjr245@gmail.com>
2022-11-07 19:14:29 +0900

    [feat] app 모듈의 build.gradle.kts 초기화 (#1)

---
+++ app/build.gradle.kts
@@ -0,0 +1,109 @@
+import org.jetbrains.kotlin.konan.properties.Properties
+
+val properties = Properties()
+properties.load(project.rootProject.file("local.properties").inputStream())
+
+plugins {
+    id("com.android.application")
+    id("kotlin-parcelize")
+    kotlin("android")
+    kotlin("kapt")
+    kotlin("plugin.serialization") version Versions.kotlinVersion
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    buildFeatures {
+        dataBinding = true
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            isShrinkResources = true
+
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    namespace = "com.yangbong.damedame"
+}
+
+dependencies {
+    implementation(project(":domain"))
+    implementation(project(":data"))
+    implementation(project(":feature:settings"))
+    implementation(project(":feature:write-diary"))
+    implementation(project(":feature:set-profile"))
+    implementation(project(":feature:friend-search"))
+    implementation(project(":shared"))
+    implementation(project(":core-ui"))
+
+    // Kotlin
+    implementation(KotlinDependencies.kotlin)
+    implementation(KotlinDependencies.kotlinxSerialization)
+    implementation(KotlinDependencies.dateTime)
+
+    // Android Core
+    implementation(AndroidXDependencies.coreKtx)
+    implementation(AndroidXDependencies.appCompat)
+    implementation(AndroidXDependencies.constraintLayout)
+    implementation(AndroidXDependencies.legacy)
+    implementation(AndroidXDependencies.coroutines)
+    implementation(AndroidXDependencies.paging3)
+    implementation(AndroidXDependencies.splashScreen)
+
+    // Material Design
+    implementation(MaterialDesignDependencies.materialDesign)
+
+    // Dagger-Hilt
+    implementation(AndroidXDependencies.hilt)
+    kapt(KaptDependencies.hiltCompiler)
+
+    // Jetpack Navigation Component
+    implementation(AndroidXDependencies.navigationFragment)
+    implementation(AndroidXDependencies.navigationUI)
+
+    // Jetpack Security
+    implementation(AndroidXDependencies.security)
+
+    // Jetpack Fragment
+    implementation(AndroidXDependencies.fragment)
+
+    // Jetpack Lifecycle
+    implementation(AndroidXDependencies.coroutines)
+    implementation(AndroidXDependencies.lifeCycleKtx)
+    implementation(AndroidXDependencies.lifecycleJava8)
+
+    // ImageLoading Library
+    // Glide for general
+    // Coil for compose
+    implementation(ThirdPartyDependencies.coil)
+
+    // Http Client Library
+    implementation(ThirdPartyDependencies.retrofit)
+    implementation(platform(ThirdPartyDependencies.okHttpBom))
+    implementation(ThirdPartyDependencies.okHttp)
+    implementation(ThirdPartyDependencies.okHttpLoggingInterceptor)
+    implementation(ThirdPartyDependencies.kotlinxSerializationConverter)
+
+    //JsonConverterLibrary
+    implementation(ThirdPartyDependencies.gson)
+    implementation(ThirdPartyDependencies.gsonConverter)
+
+    // Logger - Timber
+    implementation(ThirdPartyDependencies.timber)
+
+    // Firebase
+    implementation(platform(FirebaseDependency.firebaseBom))
+    implementation(FirebaseDependency.analyticsKtx)
+    implementation(FirebaseDependency.crashlyticsKtx)
+
+    // Automatic Record OpenSource Library List
+    implementation(ThirdPartyDependencies.ossLicense)
+
+    debugImplementation(ThirdPartyDependencies.leakCanary)
+}
+
+
