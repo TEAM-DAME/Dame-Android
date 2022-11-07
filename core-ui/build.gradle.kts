e4297f3d94e7b9e74f7a3b9b7a3ad709a600b8f2
wonseok <dnjstjr245@gmail.com>
2022-11-07 19:18:23 +0900

    [feat] core-ui 모듈의 build.gradle.kts 초기화 (#1)

---
+++ core-ui/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("kapt")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    buildFeatures {
+        dataBinding = true
+    }
+    namespace = "com.yangbong.damedame.core_ui"
+}
+
+dependencies {
+    implementation(project(":shared"))
+
+    implementation(ThirdPartyDependencies.timber)
+
+    // Android Core
+    implementation(AndroidXDependencies.coreKtx)
+    implementation(AndroidXDependencies.appCompat)
+    implementation(AndroidXDependencies.constraintLayout)
+    implementation(AndroidXDependencies.legacy)
+    implementation(AndroidXDependencies.coroutines)
+    implementation(AndroidXDependencies.fragment)
+    coreLibraryDesugaring(AndroidXDependencies.desugarLibrary)
+
+    // Dagger-Hilt
+    implementation(AndroidXDependencies.hilt)
+    kapt(KaptDependencies.hiltCompiler)
+
+    // Material Design
+    implementation(MaterialDesignDependencies.materialDesign)
+
+    // ImageLoading Library
+    implementation(ThirdPartyDependencies.coil)
+}
