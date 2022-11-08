6e0765d989aca9f5edba0429e69543a557179c07
wonseok <dnjstjr245@gmail.com>
2022-11-08 22:10:03 +0900

    [feat] MainActivity 초기 셋팅 (#1)

--- app/src/main/java/com/yangbong/damedame/MainActivity.kt
+++ app/src/main/java/com/yangbong/damedame/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.yangbong.damedame
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yangbong.core_ui.base.BindingActivity
+import com.yangbong.damedame.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
