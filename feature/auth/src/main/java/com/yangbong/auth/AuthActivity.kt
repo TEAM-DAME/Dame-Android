36a500222e19a1d8507ba8520d0229780aed447f
wonseok <dnjstjr245@gmail.com>
2022-11-11 18:56:51 +0900

    [add] KakaoLoginManager 추가 및 layout 이름 변경 (#28)

---
+++ feature/auth/src/main/java/com/yangbong/auth/AuthActivity.kt
@@ -0,0 +1,11 @@
+package com.yangbong.auth
+
+import com.yangbong.core_ui.base.BindingActivity
+import com.yangbong.damedame.set_profile.R
+import com.yangbong.damedame.set_profile.databinding.ActivityAuthBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AuthActivity : BindingActivity<ActivityAuthBinding>(R.layout.activity_auth) {
+    
+}
