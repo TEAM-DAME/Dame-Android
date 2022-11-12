d546f4fbff7ca6f693e62959d5dbf8943c3ad9d5
wonseok <dnjstjr245@gmail.com>
2022-11-12 11:28:42 +0900

    [add] core-ui 모듈 내 확장 함수, 유틸 함수 수정 및 추가 (#10)

--- core-ui/src/main/java/com/yangbong/core_ui/base/BindingActivity.kt
+++ core-ui/src/main/java/com/yangbong/core_ui/base/BindingActivity.kt
@@ -1,12 +1,16 @@
 package com.yangbong.core_ui.base
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import com.yangbong.core_ui.util.EventObserver
 import com.yangbong.core_ui.util.Injector
 import com.yangbong.damedame.navigator.MainNavigator
+
 import dagger.hilt.android.EntryPointAccessors
 
 abstract class BindingActivity<T : ViewDataBinding>(
@@ -27,4 +31,19 @@ abstract class BindingActivity<T : ViewDataBinding>(
         ).mainNavigator()
     }
 
+    private val sharedPreferences: SharedPreferences by lazy {
+        EntryPointAccessors.fromActivity(
+            this,
+            Injector.SharedPreferencesInjector::class.java
+        ).sharedPreferences()
+    }
+
+    protected fun terminationTokenHandling(viewModel: BaseViewModel) {
+        viewModel.moveToLogin.observe(this, EventObserver {
+            mainNavigator.navigateAuth(this)
+            sharedPreferences.edit { remove("DAME_DAME_ACCESS_TOKEN") }
+            sharedPreferences.edit { remove("USER_NICKNAME") }
+            finishAffinity()
+        })
+    }
 }
--- core-ui/src/main/java/com/yangbong/core_ui/util/Injector.kt
+++ core-ui/src/main/java/com/yangbong/core_ui/util/Injector.kt
@@ -1,5 +1,6 @@
 package com.yangbong.core_ui.util
 
+import android.content.SharedPreferences
 import com.yangbong.damedame.navigator.MainNavigator
 import dagger.hilt.EntryPoint
 import dagger.hilt.InstallIn
@@ -16,5 +17,11 @@ sealed interface Injector {
     @InstallIn(ActivityComponent::class)
     interface MainNavigatorInjector {
         fun mainNavigator(): MainNavigator
+    }
+
+    @EntryPoint
+    @InstallIn(ActivityComponent::class)
+    interface SharedPreferencesInjector {
+        fun sharedPreferences(): SharedPreferences
     }
 }
