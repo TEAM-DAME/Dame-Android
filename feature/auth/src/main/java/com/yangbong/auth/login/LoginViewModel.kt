cb6154d8a60b98bc9ee7a4a5bedf41f855a54b44
wonseok <dnjstjr245@gmail.com>
2022-11-13 22:41:08 +0900

    [feat] LoginViewModel 서버 연동을 제외한 비즈니스 로직 완성 (#32)

--- feature/auth/src/main/java/com/yangbong/auth/login/LoginViewModel.kt
+++ feature/auth/src/main/java/com/yangbong/auth/login/LoginViewModel.kt
@@ -5,9 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.yangbong.core_ui.util.Event
-import com.yangbong.core_ui.util.EventFlow
-import com.yangbong.core_ui.util.MutableEventFlow
-import com.yangbong.core_ui.util.asEventFlow
+import com.yangbong.domain.entity.request.DomainLoginRequest
 import com.yangbong.domain.use_case.login.LoginUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -25,6 +23,22 @@ class LoginViewModel @Inject constructor(
 
     private val _navigateToHome = MutableLiveData<Event<Boolean>>()
     val navigateToHome: LiveData<Event<Boolean>> = _navigateToHome
+
+    fun postLogin() {
+        viewModelScope.launch {
+            loginUseCases.postLogin(
+                DomainLoginRequest(
+                    platform = platform,
+                    socialToken = socialToken.value ?: ""
+                )
+            ).onSuccess {
+                loginUseCases.saveAccessToken(it.accessToken ?: "")
+            }.onFailure {
+                // TODO("실패했을 때 로직 추가")
+            }
+        }
+    }
+
 
     fun getAccessToken(): String =
         loginUseCases.getAccessToken()
