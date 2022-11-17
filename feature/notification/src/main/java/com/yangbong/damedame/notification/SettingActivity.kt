5e7b1ff668d496065f632d6346a0ec30b1c562da
boogieWoogie <kimjinuk1999@gmail.com>
2022-11-18 01:26:16 +0900

    feat(SettingActivity) : 현재 앱 버전을 확인하는 기능 구현

--- feature/notification/src/main/java/com/yangbong/damedame/notification/SettingActivity.kt
+++ feature/notification/src/main/java/com/yangbong/damedame/notification/SettingActivity.kt
@@ -1,6 +1,8 @@
 package com.yangbong.damedame.notification
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.yangbong.damedame.notification.databinding.ActivitySettingBinding
@@ -19,5 +21,23 @@ class SettingActivity : AppCompatActivity() {
         binding.settingRecycler.layoutManager=
             LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         binding.settingRecycler.adapter = adapter
+    }
+
+    private fun getCurrentAppVersion(context: Context): String {
+        var appVersion = ""
+
+        runCatching {
+            appVersion = context.packageManager
+                .getPackageInfo(context.packageName, 0)
+                .versionName
+        }.onFailure { e ->
+            Log.e("getAppVersion", e.toString())
+        }
+
+        // 문자를 제거하고 버전 숫자만 리턴하기 위해서
+        // ex)1.0.0 -> 1.0.0
+        appVersion = appVersion.replace("[a-zA-Z]|-".toRegex(), "")
+
+        return appVersion
     }
 }
