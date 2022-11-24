c2452ef15a44ffa076d6abef5dffe00ba2c5ee68
wonseok <dnjstjr245@gmail.com>
2022-11-25 02:49:18 +0900

    [fix] 회원가입 HTTP 메서드 변경 POST -> PUT (#101)

--- data/src/main/java/com/yangbong/data/remote/service/LoginService.kt
+++ data/src/main/java/com/yangbong/data/remote/service/LoginService.kt
@@ -5,11 +5,11 @@ import com.yangbong.data.remote.model.request.LoginRequest
 import com.yangbong.data.remote.model.response.BaseResponse
 import com.yangbong.data.remote.model.response.LoginResponse
 import retrofit2.http.Body
-import retrofit2.http.POST
+import retrofit2.http.PUT
 
 interface LoginService {
 
-    @POST("user/login")
+    @PUT("v1/auth/login")
     suspend fun postLogin(
         @Body body: LoginRequest
     ): NetworkState<BaseResponse<LoginResponse>>
