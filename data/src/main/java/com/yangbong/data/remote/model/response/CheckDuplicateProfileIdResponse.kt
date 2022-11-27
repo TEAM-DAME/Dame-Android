c5e891eae9b17432aa299ed2c6f6486b389a2cd2
wonseok <dnjstjr245@gmail.com>
2022-11-27 15:35:09 +0900

    [fix] BaseResponse.kt 구조 변경 (#103)

--- data/src/main/java/com/yangbong/data/remote/model/response/CheckDuplicateProfileIdResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.yangbong.data.remote.model.response
-
-import com.google.gson.annotations.SerializedName
-
-data class CheckDuplicateProfileIdResponse(
-    @SerializedName("available")
-    val available: Boolean?
-)
