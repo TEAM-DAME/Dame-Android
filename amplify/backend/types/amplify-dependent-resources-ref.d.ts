24a3de87e432651f4ae97a4cd3cf4c200ecff2ea
wonseok <dnjstjr245@gmail.com>
2022-11-28 02:26:34 +0900

    [chore] amplify 초기화 (#104)

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,19 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "damedameandroida0eb78dd": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "S3DameDame": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
