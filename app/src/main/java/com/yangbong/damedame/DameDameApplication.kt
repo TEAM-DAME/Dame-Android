0b6350c7e85f4fd28b223647846b4f6e2781b9c9
wonseok <dnjstjr245@gmail.com>
2022-11-28 02:28:22 +0900

    [chore] amplify storage 초기화 로직 추가 (#104)

--- app/src/main/java/com/yangbong/damedame/DameDameApplication.kt
+++ app/src/main/java/com/yangbong/damedame/DameDameApplication.kt
@@ -1,6 +1,11 @@
 package com.yangbong.damedame
 
 import android.app.Application
+import android.content.Context
+import com.amplifyframework.AmplifyException
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin
+import com.amplifyframework.core.Amplify
+import com.amplifyframework.storage.s3.AWSS3StoragePlugin
 import com.google.firebase.FirebaseApp
 import com.kakao.sdk.common.KakaoSdk
 import com.yangbong.core_ui.util.DameDameDebugTree
@@ -12,10 +17,34 @@ import timber.log.Timber
 class DameDameApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        if (BuildConfig.DEBUG) {
-            Timber.plant(DameDameDebugTree())
-        }
-        KakaoSdk.init(this, KAKAO_NATIVE_APP_KEY)
-        FirebaseApp.initializeApp(this)
+        initTimber()
+        initKakaoSdk(this)
+        initFirebaseApp(this)
+        initAmplify(applicationContext)
+    }
+}
+
+private fun initTimber() {
+    if (BuildConfig.DEBUG) {
+        Timber.plant(DameDameDebugTree())
+    }
+}
+
+private fun initKakaoSdk(context: Context) {
+    KakaoSdk.init(context, KAKAO_NATIVE_APP_KEY)
+}
+
+private fun initFirebaseApp(context: Context) {
+    FirebaseApp.initializeApp(context)
+}
+
+private fun initAmplify(context: Context) {
+    try {
+        Amplify.addPlugin(AWSCognitoAuthPlugin())
+        Amplify.addPlugin(AWSS3StoragePlugin())
+        Amplify.configure(context)
+        Timber.d("Initialized Amplify")
+    } catch (error: AmplifyException) {
+        Timber.d("Could not initialize Amplify $error")
     }
 }
