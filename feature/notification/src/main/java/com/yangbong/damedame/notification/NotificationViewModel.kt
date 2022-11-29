aaea4979a06edac35ee1673494db6f7ccc905a8b
boogieWoogie <kimjinuk1999@gmail.com>
2022-11-30 03:27:01 +0900

    refactor(feature/notification) : binding adapter 삭제 (#99)

--- feature/notification/src/main/java/com/yangbong/damedame/notification/NotificationViewModel.kt
+++ feature/notification/src/main/java/com/yangbong/damedame/notification/NotificationViewModel.kt
@@ -1,17 +1,10 @@
 package com.yangbong.damedame.notification
 
-import android.app.Application
-import android.widget.TextView
-import androidx.databinding.BindingAdapter
-import androidx.lifecycle.AndroidViewModel
+
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.yangbong.core_ui.base.BaseViewModel
-import com.yangbong.core_ui.extension.getString
 import com.yangbong.damedame.notification.notification_data.NotificationData
-import com.yangbong.damedame.notification.notification_data.NotificationType
-import com.yangbong.damedame.notification.notification_util.notificationMsg
 
 class NotificationViewModel : BaseViewModel() {
 
