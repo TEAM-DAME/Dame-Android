9952dd2356ce1d58cb0453a4c3ecd23742b778ef
wonseok <dnjstjr245@gmail.com>
2022-12-02 08:02:01 +0900

    [refactor] 캐릭터 설정 레포지토리에서 유저 아이디 불러오는 컨트랙트 삭제 (#129)

--- domain/src/main/java/com/yangbong/domain/repository/SetCharacterRepository.kt
+++ domain/src/main/java/com/yangbong/domain/repository/SetCharacterRepository.kt
@@ -4,7 +4,5 @@ import com.yangbong.domain.entity.request.DomainCharacterRequest
 
 interface SetCharacterRepository {
 
-    fun getUserId(): Int
-
     suspend fun postCharacter(characterRequest: DomainCharacterRequest): Result<String>
 }
