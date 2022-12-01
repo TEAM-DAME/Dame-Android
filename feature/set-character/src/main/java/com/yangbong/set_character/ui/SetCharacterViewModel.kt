7a789e63f5548f7ff84a1502a8e4af137ce8582e
wonseok <dnjstjr245@gmail.com>
2022-12-02 08:03:55 +0900

    [feat] 캐릭터 설정 비즈니스 로직 구현 및 코틀린 스타일 가이드에 맞추어 리팩토링 (#129)

--- feature/set-character/src/main/java/com/yangbong/set_character/ui/SetCharacterViewModel.kt
+++ feature/set-character/src/main/java/com/yangbong/set_character/ui/SetCharacterViewModel.kt
@@ -1,10 +1,11 @@
 package com.yangbong.set_character.ui
 
-import com.yangbong.core_ui.util.Event
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.yangbong.core_ui.base.BaseViewModel
+import com.yangbong.core_ui.util.Event
+import com.yangbong.domain.entity.CharacterInfo
 import com.yangbong.domain.entity.request.DomainCharacterRequest
 import com.yangbong.domain.repository.SetCharacterRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -17,33 +18,46 @@ class SetCharacterViewModel @Inject constructor(
     private val setCharacterRepository: SetCharacterRepository
 ) : BaseViewModel() {
 
-    private val _userId = MutableLiveData<Int>()
-    val userId: LiveData<Int> = _userId
+    private val characterList = mutableListOf<CharacterInfo>()
 
-    private val _minionId = MutableLiveData<Int>()
-    val minionId: LiveData<Int> = _minionId
-
-    private val _navigateToMainActivity = MutableLiveData<Event<Boolean>>()
-    val navigateToMainActivity: LiveData<Event<Boolean>> = _navigateToMainActivity
-
-    fun getUserId() {
-        _userId.postValue(setCharacterRepository.getUserId())
+    /**
+     * 하드코딩으로 구현 (임시)
+     */
+    init {
+        for (characterId in MIN_CHARACTER_ID..MAX_CHARACTER_ID) {
+            characterList.add(CharacterInfo(characterId))
+        }
     }
 
-    fun postCharacter(minion: Int, userId: Int) {
+    private val _navigateToHome = MutableLiveData<Event<Boolean>>()
+    val navigateToHome: LiveData<Event<Boolean>> = _navigateToHome
+
+    private val _characterInfoList = MutableLiveData<List<CharacterInfo>>()
+    val characterInfoList: LiveData<List<CharacterInfo>> = _characterInfoList
+
+    fun getCharacterList() {
+        viewModelScope.launch {
+            _characterInfoList.postValue(characterList)
+        }
+    }
+
+    fun postCharacter(userId: Int, characterId: Int) {
         viewModelScope.launch {
             setCharacterRepository.postCharacter(
                 DomainCharacterRequest(
                     userId = userId,
-                    minion = minion
+                    minion = characterId
                 )
             ).onSuccess {
-                _navigateToMainActivity.postValue(Event(true))
+                _navigateToHome.postValue(Event(true))
             }.onFailure {
                 Timber.d(it)
-                _navigateToMainActivity.postValue(Event(true))
             }
         }
     }
 
+    companion object {
+        const val MIN_CHARACTER_ID = 1
+        const val MAX_CHARACTER_ID = 9
+    }
 }
