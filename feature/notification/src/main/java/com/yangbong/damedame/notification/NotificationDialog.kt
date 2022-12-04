353a1ceb426d11dfc5717894cde428605fa25d58
boogieWoogie <kimjinuk1999@gmail.com>
2022-12-04 13:58:04 +0900

    feat(notification) : notification dialog에서 알림 시간 설정 후 확인버튼 클릭시 workmanager 작동

--- feature/notification/src/main/java/com/yangbong/damedame/notification/NotificationDialog.kt
+++ feature/notification/src/main/java/com/yangbong/damedame/notification/NotificationDialog.kt
@@ -1,4 +1,69 @@
 package com.yangbong.damedame.notification
 
-class NotificationDialog {
+import android.app.Dialog
+import android.content.ContentValues
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.util.Log
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
+import androidx.work.ExistingWorkPolicy
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import com.example.testworkmanager.NotificationWorker
+import com.yangbong.damedame.notification.databinding.UpdateVersionDialogBinding
+import com.yangbong.damedame.notification.util.getWaitingTimeBeforeNotification
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
+
+class NotificationDialog(private val context: AppCompatActivity) {
+
+    private val binding: UpdateVersionDialogBinding =
+        UpdateVersionDialogBinding.inflate(context.layoutInflater)
+
+    private val dialog = Dialog(context)
+
+    init{
+        binding.updateButton.setOnClickListener {
+            CoroutineScope(Dispatchers.Default).launch {
+                createWorkManager()
+            }
+        }
+
+        binding.cancelButton.setOnClickListener {
+            dialog.dismiss()
+        }
+    }
+
+    fun show() {
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(binding.root)
+        dialog.setCancelable(false)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        dialog.show()
+    }
+
+    private fun delayCreateWork() {
+        CoroutineScope(Dispatchers.Default).launch {
+            createWorkManager()
+        }
+    }
+
+    private fun createWorkManager() {
+
+        val oneTimeWorkRequest = OneTimeWorkRequestBuilder<NotificationWorker>().setInitialDelay(
+            getWaitingTimeBeforeNotification(
+                notificationHour = NotificationWorker.notificationHour,
+                notificationMinute = NotificationWorker.notificationMinute
+            ),
+            TimeUnit.MILLISECONDS
+        ).build()
+
+        Log.e(ContentValues.TAG, "Init WorkManager")
+        WorkManager.getInstance(context)
+            .enqueueUniqueWork("Notification Work", ExistingWorkPolicy.REPLACE, oneTimeWorkRequest)
+    }
 }
