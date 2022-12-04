f0878d94bb6f8a9b265527c9a6f10fcda9ff5346
wonseok <dnjstjr245@gmail.com>
2022-12-04 12:06:04 +0900

    [feat] 일기 작성 비즈니스 로직 구현 (#138)

--- feature/write-diary/src/main/java/com/yangbong/write_diary/WriteDiaryActivity.kt
+++ feature/write-diary/src/main/java/com/yangbong/write_diary/WriteDiaryActivity.kt
@@ -1,7 +1,11 @@
 package com.yangbong.write_diary
 
+import android.os.Bundle
 import androidx.activity.viewModels
 import com.yangbong.core_ui.base.BindingActivity
+import com.yangbong.core_ui.extension.setOnSingleClickListener
+import com.yangbong.core_ui.util.EventObserver
+import com.yangbong.damedame.shared.custom.DameDameDialogFragment
 import com.yangbong.damedame.write_diary.R
 import com.yangbong.damedame.write_diary.databinding.ActivityWriteDiaryBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,5 +16,71 @@ class WriteDiaryActivity :
 
     private val writeDiaryViewModel by viewModels<WriteDiaryViewModel>()
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        writeDiaryViewModel.getUserId()
+        initClickEvent()
+        initPostDiaryObserver()
+        initMoveToHomeObserver()
+    }
+
+    private fun initClickEvent() {
+        binding.btnBack.setOnSingleClickListener {
+            showDialog(
+                title = getString(com.yangbong.damedame.shared.R.string.exit_diary_dialog_title),
+                body = getString(com.yangbong.damedame.shared.R.string.exit_diary_dialog_content),
+                positiveButtonClickListener = { finish() }
+            )
+        }
+        binding.btnComplete.setOnSingleClickListener {
+            showDialog(
+                title = getString(com.yangbong.damedame.shared.R.string.post_diary_dialog_title),
+                body = getString(com.yangbong.damedame.shared.R.string.post_diary_dialog_content),
+                positiveButtonClickListener = ::startSentimentAnalyze
+            )
+        }
+    }
+
+    private fun showDialog(title: String, body: String, positiveButtonClickListener: () -> Unit) {
+        DameDameDialogFragment.newInstance(
+            title = title,
+            body = body,
+            positiveButtonClickListener = positiveButtonClickListener
+        ).show(
+            supportFragmentManager,
+            DameDameDialogFragment.TAG
+        )
+    }
+
+    private fun startSentimentAnalyze() {
+        writeDiaryViewModel.postSentiment(binding.etContent.text.toString())
+    }
+
+    private fun initPostDiaryObserver() {
+        writeDiaryViewModel.postDiary.observe(
+            this,
+            EventObserver {
+                postDiary()
+            }
+        )
+    }
+
+    private fun postDiary() {
+        writeDiaryViewModel.postDiary(writeDiaryViewModel.userId.value ?: -1)
+    }
+
+    private fun initMoveToHomeObserver() {
+        writeDiaryViewModel.moveToHome.observe(
+            this,
+            EventObserver {
+                moveMainActivity()
+            }
+        )
+    }
+
+    private fun moveMainActivity() {
+        mainNavigator.navigateMain(this)
+        finish()
+    }
 
 }
