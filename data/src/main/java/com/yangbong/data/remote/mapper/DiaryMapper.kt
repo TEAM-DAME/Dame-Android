3e46bdb0ae58dd059b3a786c64dd6689d69f5cf9
wonseok <dnjstjr245@gmail.com>
2022-12-05 03:58:58 +0900

    [feat] DiaryMapper 클래스 정의 (#147)

--- data/src/main/java/com/yangbong/data/remote/mapper/DiaryMapper.kt
+++ data/src/main/java/com/yangbong/data/remote/mapper/DiaryMapper.kt
@@ -1,29 +1,22 @@
 package com.yangbong.data.remote.mapper
 
-import com.yangbong.data.local.data_source.LocalPreferenceUserDataSource
 import com.yangbong.data.remote.model.response.Diary
-import com.yangbong.data.remote.model.response.EmotionTypeWithValue
 import com.yangbong.domain.entity.DiaryInfo
-import com.yangbong.domain.entity.EmotionInfo
 import javax.inject.Inject
 
-class DiaryMapper @Inject constructor(
-    val dataSource: LocalPreferenceUserDataSource
-) {
-    fun toDiaryInfo(diary: Diary): DiaryInfo {
-        return DiaryInfo(
-            id = diary.id,
-            emotion = toEmotionInfo(diary.emotionTypeWithValue),
-            title = diary.title,
-            date = diary.date,
-            isLocked = diary.isLocked
-        )
-    }
+class DiaryMapper @Inject constructor() {
 
-    private fun toEmotionInfo(emotionTypeWithValue: EmotionTypeWithValue): EmotionInfo {
-        return EmotionInfo(
-            emotionType = emotionTypeWithValue.emotionType,
-            emotionValue = emotionTypeWithValue.emotionValue
+    fun toDiaryInfo(diaryItem: Diary): DiaryInfo =
+        DiaryInfo(
+            diaryId = diaryItem.diaryId,
+            userId = diaryItem.userId,
+            title = diaryItem.title,
+            content = diaryItem.content,
+            positive = diaryItem.positive,
+            neutral = diaryItem.neutral,
+            negative = diaryItem.negative,
+            visibility = diaryItem.visibility,
+            diaryTime = diaryItem.diaryTime,
+            minionId = diaryItem.minionId
         )
-    }
 }
