b8f65c31b1d3308f4dd6917459ee73990ae18eb8
wonseok <dnjstjr245@gmail.com>
2022-12-05 11:18:00 +0900

    [feat] Jetpack Navigation으로의 migration으로 인한 코드 수정 (#155)

--- app/src/main/java/com/yangbong/damedame/MainActivity.kt
+++ app/src/main/java/com/yangbong/damedame/MainActivity.kt
@@ -2,60 +2,25 @@ package com.yangbong.damedame
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.yangbong.core_ui.base.BindingActivity
 import com.yangbong.core_ui.extension.setOnSingleClickListener
-import com.yangbong.damedame.adapter.MainViewPagerAdapter
 import com.yangbong.damedame.databinding.ActivityMainBinding
 import com.yangbong.damedame.factory.DameDameFragmentFactory
-import com.yangbong.main.home.HomeFragment
-import com.yangbong.main.my_profile.MyProfileFragment
-import com.yangbong.main.search.SearchFragment
 import com.yangbong.write_diary.WriteDiaryActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
 
-    private lateinit var mainViewPagerAdapter: MainViewPagerAdapter
-
     override fun onCreate(savedInstanceState: Bundle?) {
         supportFragmentManager.fragmentFactory = DameDameFragmentFactory(this)
         super.onCreate(savedInstanceState)
-        initAdapter()
-        syncBottomNavWithVp()
-        initFabWriteClickListener()
         initTransactionToHome()
-    }
-
-    private fun initAdapter() {
-        binding.vpMain.adapter = MainViewPagerAdapter(this).also { mainViewPagerAdapter = it }
-        mainViewPagerAdapter.fragmentList = listOf(
-            supportFragmentManager.fragmentFactory.instantiate(
-                classLoader,
-                HomeFragment::class.java.name
-            ),
-            supportFragmentManager.fragmentFactory.instantiate(
-                classLoader,
-                SearchFragment::class.java.name
-            ),
-            supportFragmentManager.fragmentFactory.instantiate(
-                classLoader,
-                MyProfileFragment::class.java.name
-            )
-        )
-    }
-
-    private fun syncBottomNavWithVp() {
-        binding.vpMain.isUserInputEnabled = false
-
-        binding.bottomNav.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.menu_home -> binding.vpMain.setCurrentItem(0, false)
-                R.id.menu_search -> binding.vpMain.setCurrentItem(1, false)
-                R.id.menu_my_profile -> binding.vpMain.setCurrentItem(2, false)
-            }
-            return@setOnItemSelectedListener true
-        }
+        initFabWriteClickListener()
+        syncBottomNavWithNavController()
     }
 
     private fun initFabWriteClickListener() {
@@ -65,14 +30,24 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
         }
     }
 
+    private fun syncBottomNavWithNavController() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        findViewById<BottomNavigationView>(R.id.bottom_nav)
+            .setupWithNavController(navController)
+    }
+
     private fun initTransactionToHome() {
         transactionToHome = {
-            binding.vpMain.setCurrentItem(0, false)
-            binding.bottomNav.selectedItemId = R.id.menu_home
+            findViewById<BottomNavigationView>(R.id.bottom_nav)
+                .selectedItemId = R.menu.menu_main
         }
     }
 
     companion object {
         lateinit var transactionToHome: () -> Unit
     }
+
+
 }
